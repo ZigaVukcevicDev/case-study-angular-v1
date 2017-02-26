5e51117bde1459ffc9aa45a50ed63cc837d9b4dd
Ziga Vukcevic <ziga@be-codified.com>
2017-02-26 16:33:59 +0100

    Update

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,60 +1,69 @@
 //  Add ui-router as a dependency
 angular.module('app', ['ui.router']);
-angular.module('app').config(function($stateProvider, $urlRouterProvider){
+angular
+	.module('app')
+    .config(function($stateProvider, $urlRouterProvider) {
 
-    //  If a user goes to an url that doesn't have a valid state assigned
+    // If a user goes to an url that doesn't have a valid state assigned
 	$urlRouterProvider.otherwise('/error');
 
-	$stateProvider.state('home',
-	{
-		url: '/',
-		template: '<h1>Homepage</h1>'
-	});
+	$stateProvider.state(
+	    'home', {
+		    url: '/',
+		    template: '<h1>Homepage</h1>'
+        }
+    );
 
-	$stateProvider.state('login',
-	{
-		url: '/login',
-		template: '<h1>Login</h1>'
-	});
+	$stateProvider.state(
+	    'router', {
+		    url: '/router',
+            templateUrl: 'templates/template-router.html'
+	    }
+    );
 
-	$stateProvider.state('error',
-	{
-		url: '/error',
-		template: '<h2>Error 404</h2>'
-	});
+	$stateProvider.state(
+	    'error', {
+		    url: '/error',
+		    template: '<h2>Error 404</h2>'
+	    }
+    );
 
-	$stateProvider.state('parent',
-	{
-		url: '/parent',
-		template: '<h1>Parent state <span class="text-muted"><small>Has an additional ui-view.</small></span></h1><ui-view></ui-view>'
-	});
+	$stateProvider.state(
+	    'parent', {
+		    url: '/parent',
+		    template: '<h1>Parent state <span class="text-muted"><small>Has an additional ui-view.</small></span></h1><ui-view></ui-view>'
+	    }
+    );
 
-	$stateProvider.state('parent.child', 
-	{
-		url: '/child',
-		template: '<div class="well"><h4>Child state</h4></div>'
-	})
+	$stateProvider.state(
+	    'parent.child', {
+		    url: '/child',
+		    template: '<div class="well"><h4>Child state</h4></div>'
+	    }
+    );
 
-	$stateProvider.state('parameter', {
-		url: '/parameter/:name',
-		template: '<h1>Parameter state with a name parameter</h1><p>Name is : {{ name }}</p>',
-		controller: function($scope, $stateParams, $state){
-            //  Use $stateParams to get url parameters
-            $scope.name = $stateParams.name;
+	$stateProvider.state(
+	    'parameter', {
+		    url: '/parameter/:name',
+		    template: '<h1>Parameter state with a name parameter</h1><p>Name is : {{ name }}</p>',
+		    controller: function($scope, $stateParams, $state) {
 
-            //  If you want to redirect to a state
-            //  $state.go('login');
+		        //  Use $stateParams to get url parameters
+                $scope.name = $stateParams.name;
 
-            //  If you want to reload a state
-			//  $state.reload();
+                //  If you want to redirect to a state
+                //  $state.go('login');
 
-            //  Check if the current active state is...
-            // $state.is('login');
-		}
-	});
+                //  If you want to reload a state
+                //  $state.reload();
+
+                //  Check if the current active state is...
+                // $state.is('login');
+		    }
+	    }
+    );
 
 });
-
 
 angular.module('app').controller('ExampleController', function($scope){
 
